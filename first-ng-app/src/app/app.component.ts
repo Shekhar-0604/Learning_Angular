697dc017a6c572f0ba07d1161a01f06e47911760
Shekhar <Shekhar209203@gmail.com>
2025-06-07 23:38:24 +0530

    changes

--- first-ng-app/src/app/app.component.ts
+++ first-ng-app/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 import { CounterComponent } from './components/counter/counter.component';
+import { MainComponent } from './main/main.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, CounterComponent],
+  imports: [RouterOutlet, HeaderComponent, CounterComponent, MainComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
