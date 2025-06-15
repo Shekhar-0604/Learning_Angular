5523433ac9b39d3b3cb936536cc7e01d87abc46c
Shekhar <Shekhar209203@gmail.com>
2025-06-15 23:35:16 +0530

    just to do commit

--- second-ng-app/src/app/app.component.ts
+++ second-ng-app/src/app/app.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
+  standalone: true,
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'second-ng-app';
+
+  handleEvent(event: any) {
+    console.log('Function Handle Event Called', event.type);
+    //console.log('values', (event.target as HTMLInputElement).value);
+  }
 }
