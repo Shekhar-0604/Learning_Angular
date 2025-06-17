b391372c9cd5f54c4b678699e902a347142c8b30
Shekhar <Shekhar209203@gmail.com>
2025-06-17 15:32:34 +0530

    changes in learning

--- second-ng-app/src/app/app.component.ts
+++ second-ng-app/src/app/app.component.ts
@@ -4,15 +4,36 @@ import { RouterOutlet } from '@angular/router';
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'second-ng-app';
+  // title = 'second-ng-app';
 
-  handleEvent(event: any) {
-    console.log('Function Handle Event Called', event.type);
-    //console.log('values', (event.target as HTMLInputElement).value);
+  // handleEvent(event: any) {
+  //   console.log('Function Handle Event Called', event.type);
+  //   //console.log('values', (event.target as HTMLInputElement).value);
+  // }
+  name = '';
+  displayName = '';
+  getName(event: Event) {
+    const name = (event.target as HTMLInputElement).value;
+    console.log(name);
+    this.name = name;
+  }
+  setName() {
+    this.displayName = 'Shah';
+  }
+  showName() {
+    this.displayName = this.name;
+  }
+
+  email = '';
+
+  getEmail(val: string) {
+    this.email = val;
+  }
+  setEmail() {
+    this.email = 'random@gmail.com';
   }
 }
