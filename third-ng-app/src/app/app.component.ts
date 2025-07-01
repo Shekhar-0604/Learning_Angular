5ed07519fb55471cf2d345a874ed339237b625e8
Shekhar <Shekhar209203@gmail.com>
2025-07-01 21:10:45 +0530

    Learn

--- third-ng-app/src/app/app.component.ts
+++ third-ng-app/src/app/app.component.ts
@@ -1,15 +1,24 @@
-import { NgIf } from '@angular/common';
+import {
+  CommonModule,
+  NgFor,
+  NgIf,
+  NgSwitch,
+  NgSwitchCase,
+  NgSwitchDefault,
+} from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   standalone: true,
   selector: 'app-root',
-  imports: [NgIf],
+  imports: [NgIf, NgFor, NgSwitch, NgSwitchCase, NgSwitchDefault, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'third-ng-app';
-  show = false;
+  show = true;
+  user = ['Amit', 'Shah'];
+  nSwit = 'A';
 }
