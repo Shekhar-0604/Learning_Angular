1f59e9abee7e5b97d7827c6e6b50a0a68341faa7
Shekhar <Shekhar209203@gmail.com>
2025-07-05 23:50:22 +0530

    Learning going on

--- fourth-ng-app/src/app/app.component.ts
+++ fourth-ng-app/src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, RouterLink, RouterOutlet],
+  imports: [HeaderComponent, RouterLink, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'fourth-ng-app';
+
+  addDetails(val: NgForm) {
+    console.log(val);
+  }
 }
--- fourth-ng-app/src/app/home/home.component.ts
+++ fourth-ng-app/src/app/home/home.component.ts
@@ -1,28 +1,68 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [RouterLink, ReactiveFormsModule, NgIf],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
-  users = [
-    {
-      id: '1',
-      name: 'Shekhar Sharma',
-      age: 21,
-    },
-    {
-      id: '2',
-      name: 'Abhishek Sharma',
-      age: 22,
-    },
-    {
-      id: '3',
-      name: 'Ritik Sharma',
-      age: 23,
-    },
-  ];
+  // constructor(private router: Router) {}
+  // goToProfile() {
+  //   this.router.navigate(['profile'], { queryParams: { name: 'shah' } });
+  // }
+  // users = [
+  //   {
+  //     id: '1',
+  //     name: 'Shekhar Sharma',
+  //     age: 21,
+  //   },
+  //   {
+  //     id: '2',
+  //     name: 'Abhishek Sharma',
+  //     age: 22,
+  //   },
+  //   {
+  //     id: '3',
+  //     name: 'Ritik Sharma',
+  //     age: 23,
+  //   },
+  // ];
+
+  // name = new FormControl();
+  // passwprd = new FormControl();
+
+  // display() {
+  //   console.log(this.name.value, this.passwprd.value);
+  // }
+
+  profileDetails = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(5),
+    ]),
+    email: new FormControl('', [Validators.required, Validators.maxLength(50)]),
+  });
+  get name() {
+    return this.profileDetails.get('name');
+  }
+  get password() {
+    return this.profileDetails.get('password');
+  }
+  get email() {
+    return this.profileDetails.get('email');
+  }
+  display() {
+    console.log(this.profileDetails.value);
+  }
+  submitData() {}
 }
