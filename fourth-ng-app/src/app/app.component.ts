868110c75c09e522e775518207726126dae0f891
Shekhar <Shekhar209203@gmail.com>
2025-07-06 21:18:52 +0530

    Learning going on

--- fourth-ng-app/src/app/app.component.ts
+++ fourth-ng-app/src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { FormsModule, NgForm } from '@angular/forms';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent, RouterLink, RouterOutlet, FormsModule],
+  imports: [HeaderComponent, RouterLink, RouterOutlet, FormsModule, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
